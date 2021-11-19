78c96d9335147f090a03b488f9b2eff004e5df67
bla5940ck <bla5940ck@gmail.com>
2021-11-19 18:11:27 +0800

    1

--- src/web/product/model/ProdCategoryDAOImpl.java
+++ src/web/product/model/ProdCategoryDAOImpl.java
@@ -1,5 +1,10 @@
 package web.product.model;
 
-public interface ProdCategoryImpl {
+import java.util.List;
+
+public interface ProdCategoryDAOImpl {
+	public ProdCategoryVO findCategoryByPK(int id);
+	public List<ProdCategoryVO> getAllCategory();
+	public void addCategory(ProdCategoryVO prodCategory);
 
 }
---
+++ src/web/product/model/ProdCategoryImpl.java
@@ -0,0 +1,5 @@
+package web.product.model;
+
+public interface ProdCategoryImpl {
+
+}
