3502bbbb2e256568588f8b399715a83b98325dc5
leoshin89 <a1276861940809@gmail.com>
2021-11-22 17:23:57 +0800

    no message

--- src/web/promo_list/model/testlist.java
+++ src/web/promo_list/model/testlist.java
@@ -7,27 +7,29 @@ public class testlist {
 		Promolist_impl dao = new PromolistDAO();
 		PromolistVO promolistVO = new PromolistVO();
 		
-//		promolistVO.setPromo_id(103);
-//		promolistVO.setCategory_id(3);
-//		promolistVO.setCoupon_name("ddd");
-//		promolistVO.setDiscount(20.00);
-//		promolistVO.setAmount(20);
-//		promolistVO.setUsed(0);
-//		promolistVO.setStart_date(java.sql.Date.valueOf("2016-08-07"));
-//		promolistVO.setEnd_date(java.sql.Date.valueOf("2016-08-07"));
-//		dao.insert(promolistVO);
+		// coupon_id, promo_id, category_id, coupon_name, discount, amount, used, start_date, end_date
 		
-		PromolistVO emp = dao.findByPrimaryKey(1002);
-		System.out.print(emp.getCoupon_id() + ",");
-		System.out.print(emp.getPromo_id() + ",");
-		System.out.print(emp.getCategory_id() + ",");
-		System.out.print(emp.getCoupon_name() + ",");
-		System.out.print(emp.getDiscount() + ",");
-		System.out.print(emp.getAmount() + ",");
-		System.out.print(emp.getUsed() + ",");
-		System.out.print(emp.getStart_date() + ",");
-		System.out.print(emp.getEnd_date() + ",");
-		System.out.println();
+		promolistVO.setPromo_id(103);
+		promolistVO.setCategory_id(3);
+		promolistVO.setCoupon_name("ddd");
+		promolistVO.setDiscount(20.00);
+		promolistVO.setAmount(20);
+		promolistVO.setUsed(0);
+		promolistVO.setStart_date(java.sql.Date.valueOf("2016-08-07"));
+		promolistVO.setEnd_date(java.sql.Date.valueOf("2016-08-07"));
+		dao.insert(promolistVO);
+		
+//		PromolistVO emp = dao.findByPrimaryKey(1002);
+//		System.out.print(emp.getCoupon_id() + ",");
+//		System.out.print(emp.getPromo_id() + ",");
+//		System.out.print(emp.getCategory_id() + ",");
+//		System.out.print(emp.getCoupon_name() + ",");
+//		System.out.print(emp.getDiscount() + ",");
+//		System.out.print(emp.getAmount() + ",");
+//		System.out.print(emp.getUsed() + ",");
+//		System.out.print(emp.getStart_date() + ",");
+//		System.out.print(emp.getEnd_date() + ",");
+//		System.out.println();
 		
 //		List<PromolistVO> list = dao.getAll();
 //		for (PromolistVO emp : list) {
